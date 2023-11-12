f84e19b09b2e40e2bf82f81ec64643e96194f245
dtorresss <diegotorresprofesional@gmail.com>
2023-11-12 18:25:52 +0100

    Sapphire, Raw Sapphire and Mod Blocks Tab added

--- TutorialMod/forge-1.20.1-47.2.0-mdk/src/main/java/net/diego/tutorialmod/TutorialMod.java
+++ TutorialMod/forge-1.20.1-47.2.0-mdk/src/main/java/net/diego/tutorialmod/TutorialMod.java
@@ -1,6 +1,10 @@
 package net.diego.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.diego.tutorialmod.item.ModCreativeModTabs;
+import net.diego.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,7 +17,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
@@ -22,6 +25,10 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -36,15 +43,16 @@ public class TutorialMod
     }
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
 
     }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
